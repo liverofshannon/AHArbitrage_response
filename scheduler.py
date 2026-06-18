a46533aa33207dc92f7f4fe04a981d955a7801df
Harry <2529223001@qq.com>
2026-06-18 23:19:55 +0800

    汇率

--- scheduler.py
+++ scheduler.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-交易日 9:24 获取参考汇率，16:10 获取结算汇兑比率，写入 AH_EXCHG_RATE
+工作日北京时间 9:24 拉参考汇率，16:10 拉结算汇兑比率，存入 AH_EXCHG_RATE
 """
 
 import os
-from zoneinfo import ZoneInfo
+from datetime import timezone, timedelta
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from logger import get_logger
 
+from logger import get_logger
 import sse_reference_rate
 from wecom_alert import send_alert
 
 logger = get_logger()
 
-TZ = ZoneInfo("Asia/Shanghai")
-TRIGGER_DAILY = dict(day_of_week="mon-fri", timezone=TZ)
+TZ = timezone(timedelta(hours=8))
+TRIGGER = dict(day_of_week="mon-fri", timezone=TZ)
 
 
 class ExchangeRateScheduler:
@@ -25,7 +25,7 @@ class ExchangeRateScheduler:
     def __init__(self):
         self._scheduler = BackgroundScheduler(timezone=TZ)
 
-    def _update_rate(self, label, fetch_fn):
+    def _update(self, label, fetch_fn):
         try:
             rate = fetch_fn()
             if rate:
@@ -37,27 +37,19 @@ class ExchangeRateScheduler:
         except Exception as e:
             logger.error("%s failed: %s", label, e)
 
-    def _fetch_reference(self):
-        self._update_rate("参考汇率", sse_reference_rate.fetch_reference_rate)
-
-    def _fetch_settlement(self):
-        self._update_rate("结算汇兑比率", sse_reference_rate.fetch_settlement_rate)
-
     def start(self):
         self._scheduler.add_job(
-            self._fetch_reference,
-            CronTrigger(hour=9, minute=24, **TRIGGER_DAILY),
+            lambda: self._update("参考汇率", sse_reference_rate.fetch_reference_rate),
+            CronTrigger(hour=9, minute=24, **TRIGGER),
             id="reference_rate",
         )
         self._scheduler.add_job(
-            self._fetch_settlement,
-            CronTrigger(hour=16, minute=10, **TRIGGER_DAILY),
+            lambda: self._update("结算汇兑比率", sse_reference_rate.fetch_settlement_rate),
+            CronTrigger(hour=16, minute=10, **TRIGGER),
             id="settlement_rate",
         )
-        # 启动时拉一次参考汇率
-        self._fetch_reference()
         self._scheduler.start()
-        logger.info("scheduler started: reference_rate@9:24, settlement_rate@16:10")
+        logger.info("scheduler started: reference_rate@9:24, settlement_rate@16:10 (UTC+8)")
 
     def stop(self):
         self._scheduler.shutdown(wait=False)
